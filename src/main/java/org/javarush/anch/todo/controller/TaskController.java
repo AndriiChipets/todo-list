d1622cb37098a7096ca99d5d6ba50afd1263eaee
Andrii Chipets <andrii.chipets@gmail.com>
2024-04-13 17:17:50 +0300

    finished controller layer implementation

--- src/main/java/org/javarush/anch/todo/controller/TaskController.java
+++ src/main/java/org/javarush/anch/todo/controller/TaskController.java
@@ -1,11 +1,16 @@
 package org.javarush.anch.todo.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.javarush.anch.todo.domain.Task;
 import org.javarush.anch.todo.dto.TaskInfo;
 import org.javarush.anch.todo.service.TaskService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+import static java.util.Objects.isNull;
 
 @Controller
 @RequestMapping("/")
@@ -19,6 +24,8 @@ public class TaskController {
                         @RequestParam(value = "page", required = false, defaultValue = "1") int page,
                         @RequestParam(value = "limit", required = false, defaultValue = "10") int limit) {
 
+        List<Task> tasks = taskService.getAll((page - 1) * limit, limit);
+        model.addAttribute("tasks", tasks);
         return "tasks";
     }
 
@@ -26,19 +33,26 @@ public class TaskController {
     public void edit(Model model,
                      @PathVariable Integer id,
                      @RequestBody TaskInfo info) {
-
+        if (isNull(id) || id <= 0) {
+            throw new RuntimeException("Invalid id");
+        }
+        taskService.edit(id, info.getDescription(), info.getStatus());
     }
 
     @PostMapping("/")
     public void add(Model model,
                     @RequestBody TaskInfo info) {
 
+        taskService.create(info.getDescription(), info.getStatus());
     }
 
     @PostMapping("/{id}")
     public String delete(Model model,
                          @PathVariable Integer id) {
-
+        if (isNull(id) || id <= 0) {
+            throw new RuntimeException("Invalid id");
+        }
+        taskService.delete(id);
         return "tasks";
     }
 }
